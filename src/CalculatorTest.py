a985718e1a377f12e578c8b3cdf9775fe6a19710
wmd4@njit.edu <wmd4@njit.edu>
2021-07-10 20:24:52 -0400

    Task: self setup to remove repeating calculator.

---
+++ src/CalculatorTest.py
@@ -0,0 +1,25 @@
+import unittest
+from Calculator import Calculator
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_results_property_calculator(self):
+        self.assertEqual(self.calculator.result, 0)
+
+    def test_add_method_calculator(self):
+        self.assertEqual(self.calculator.add(2, 2), 4)
+        self.assertEqual(self.calculator.result, 4)
+
+    def test_subtract_method_calculator(self):
+        self.assertEqual(self.calculator.subtract(2, 2), 0)
+        self.assertEqual(self.calculator.result, 0)
+
+if __name__ == '__main__':
+    unittest.main()
